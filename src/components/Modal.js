9159d69d180705d04b1b4d740b4a39a0cbded284
ReneHartjes <renehartjes@web.de>
2021-09-24 14:16:33 +0200

    ModalBigger

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -19,9 +19,11 @@ export default function Modal({opener}) {
                 <h3>Annoying Modal</h3>
                 
 
-                <p> I dont want cookies or anything, just wanna annoy you with this semi-cool modal</p>
+                <p> I dont want cookies or anything, just wanna annoy you with this semi-cool Modal/Popup</p>
 
-                <button onClick ={()=> opener(false)}>Close</button>
+                <button onClick ={()=> opener(false)}>Acknowledge</button>
+
+                
 
             </div>
             
